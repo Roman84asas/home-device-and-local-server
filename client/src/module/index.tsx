e7e1dcb1eee6092d23a9c9c6f15374014ec3a96e
RomanMereneanu <asasin84@mail.ru>
2020-04-05 21:46:51 +0300

    add all project

---
+++ client/src/module/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import "./iiii.scss";
+
+function HelloWorld() {
+    return (
+        <div className="divvvv">Hello world</div>
+    )
+}
+export default HelloWorld;
