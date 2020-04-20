814ba8f319dd0eef9564fad0e0142cc0e3182474
RomanMereneanu <asasin84@mail.ru>
2020-04-20 19:00:22 +0300

    add all project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hy
+      </header>
+    </div>
+  );
+}
+
+export default App;
