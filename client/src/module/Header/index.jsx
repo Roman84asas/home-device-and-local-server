40ae589b31b4d16b4c098a62342757fb591b8e37
RomanMereneanu <asasin84@mail.ru>
2020-04-22 23:05:30 +0300

    add a

--- client/src/module/Header/index.jsx
+++ client/src/module/Header/index.jsx
@@ -7,11 +7,12 @@ const Header = () => {
     return(
         <header>
             <div className="ui menu">
-                <Image src='https://sun9-22.userapi.com/c858132/v858132878/19a68c/JTq9xrYhWk4.jpg?ava=1' size='mini' circular className="logog_foto"/>
-                <div className="full_name">Roman</div>
-                <div className="name_ai">Your AI:
-                    <span>Anubis</span>
+
+                <Image src='https://sun9-22.userapi.com/c858132/v858132878/19a68c/JTq9xrYhWk4.jpg?ava=1' size='mini' circular className="logo_foto"/>
+                <div className="left menu full_name">
+                    <span>Roman</span>
                 </div>
+
                 <div className="right menu">
                     <div className="item">
                         <button className="ui violet basic button">Log-in</button>
