5e4c5592aaa50d30160c23cbe4c1a1f83d78d663
RomanMereneanu <asasin84@mail.ru>
2020-04-23 20:39:51 +0300

    add all project

--- client/src/module/Header/index.jsx
+++ client/src/module/Header/index.jsx
@@ -7,12 +7,10 @@ const Header = () => {
     return(
         <header>
             <div className="ui menu">
-
                 <Image src='https://sun9-22.userapi.com/c858132/v858132878/19a68c/JTq9xrYhWk4.jpg?ava=1' size='mini' circular className="logo_foto"/>
                 <div className="left menu full_name">
                     <span>Roman</span>
                 </div>
-
                 <div className="right menu">
                     <div className="item">
                         <button className="ui violet basic button">Log-in</button>
