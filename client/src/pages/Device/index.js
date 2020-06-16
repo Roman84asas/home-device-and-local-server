3e10e4a81c089caba9cbd4269c0ee3053bd2f02c
RomanMereneanu <asasin84@mail.ru>
2020-06-16 19:09:38 +0300

    New

--- client/src/pages/Device/index.js
+++ client/src/pages/Device/index.js
@@ -10,9 +10,26 @@ const Device = () => {
         <div className="content">
             <Header/>
             <main className="device_content">
-                <div className="title">
-                    <h1>Device </h1>
-                </div>
+                <div className="ui grid content_device">
+                    <div className="four wide column">
+                        <div className="ui fluid vertical tabular menu">
+                            <a className="active item" href="/#">Camera</a>
+                            <a className="item" href="/#">Kitchen</a>
+                            <a className="item" href="/#">Bedroom</a>
+                            <a className="item" href="/#">Restroom</a>  
+                            <a className="item" href="/#">Gym</a>  
+                            <a className="item" href="/#">Salon</a>
+                            <a className="item" href="/#">Cinema hall</a>
+                            <a className="item" href="/#">pool</a>
+                            <a className="item" href="/#">Bathroom</a>                        
+                        </div>
+                    </div>
+                    <div className="stretched twelve wide column">
+                        <div className="ui segment">
+                        This is an stretched grid column. This segment will always match the tab height
+                        </div>
+                    </div>
+                </div>    
             </main>
         </div>
     )
